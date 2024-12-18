50e8c4f37ba4496cc29fada10b6c8bb3eaa3f19e
Garik-ctrl <garik.minasjan@gmail.com>
2024-12-18 20:34:17 +0100

    Add files via upload

---
+++ 2024-12-14 functions.py
@@ -0,0 +1,7 @@
+def log_into_terminal():
+    local_text="Dneska je neděle"
+    print("Globální proměnná :", this_text) #Př.1
+    print("Lokální proměnná :", local_text) #Př.2
+
+this_text="Dneska je pondělí"
+log_into_terminal()
